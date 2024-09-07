2dd7907fc0d5b9d65cc3e5c1544fc9997f2d3ba6
PascalAllekotte <pascalallekotte1@web.de>
2024-09-08 00:10:57 +0200

    XXX

---
+++ CustomTabBars/CustomTabBar1.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBar1.swift
+//  CustomTabBars
+//
+//  Created by Pascal Allekotte on 07.09.24.
+//
+
+import SwiftUI
+
+struct CustomTabBar1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBar1()
+}
---
+++ CustomTabBars/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  CustomTabBars
+//
+//  Created by Pascal Allekotte on 07.09.24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
