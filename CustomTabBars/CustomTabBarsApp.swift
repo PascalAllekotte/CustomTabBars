bc833f4b7477c7197c31ce7b1e3dbe1a29aa0443
PascalAllekotte <pascalallekotte1@web.de>
2024-09-07 15:32:53 +0200

    Initial Commit

---
+++ CustomTabBars/CustomTabBarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarsApp.swift
+//  CustomTabBars
+//
+//  Created by Pascal Allekotte on 07.09.24.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
